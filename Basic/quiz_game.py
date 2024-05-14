304f79e5b3a6e055221b985c8f7b38073623575e
Kavin T <tdskavinkalkin@gmail.com>
2024-05-14 07:08:50 +0530

    basic_quiz_game

---
+++ Basic/quiz_game.py
@@ -0,0 +1,30 @@
+class Questions:
+
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+
+
+questions_prompt = [
+    "who is kavin ?\na)Human\nb)alien\nc)dog\n",
+    "who is kavin ?\na)Human\nb)alien\nc)dog\n",
+    "who is kavin ?\na)Human\nb)alien\nc)dog\n"
+]
+
+questions = [
+    Questions(questions_prompt[0], "a"),
+    Questions(questions_prompt[1], "a"),
+    Questions(questions_prompt[2], "a")
+]
+
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got " + str(score) + "/" + str(len(questions)))
+
+
+run_test(questions)
